0b04075cf5d98a07b04fb2cdf9864e48bb189607
Jonathan Zanella <jonathanrafaelz@gmail.com>
2020-12-10 14:21:01 -0300

    auth

--- src/app.exercise.js
+++ src/app.exercise.js
@@ -3,25 +3,66 @@ import {jsx} from '@emotion/core'
 
 import * as React from 'react'
 import * as auth from 'auth-provider'
+import {FullPageSpinner} from './components/lib'
+import {client} from './utils/api-client'
+import {useAsync} from './utils/hooks'
+import * as colors from './styles/colors'
 import {AuthenticatedApp} from './authenticated-app'
 import {UnauthenticatedApp} from './unauthenticated-app'
 
-function App() {
-  const [user, setUser] = React.useState(null)
+async function getUser() {
+  let user = null
 
-  const login = form => auth.login(form).then(u => setUser(u))
-  const register = form => auth.register(form).then(u => setUser(u))
-
-  const logout = () => {
-    auth.logout()
-    setUser(null)
+  const token = await auth.getToken()
+  if (token) {
+    const data = await client('me', {token})
+    user = data.user
   }
 
-  return user ? (
-    <AuthenticatedApp user={user} logout={logout} />
-  ) : (
-    <UnauthenticatedApp login={login} register={register} />
-  )
+  return user
+}
+
+function App() {
+  const {
+    data: user,
+    error,
+    isLoading,
+    isIdle,
+    isError,
+    isSuccess,
+    run,
+    setData,
+  } = useAsync()
+
+  React.useEffect(() => {
+    run(getUser())
+  }, [run])
+
+  const login = form => auth.login(form).then(u => setData(u))
+  const register = form => auth.register(form).then(u => setData(u))
+  const logout = () => {
+    auth.logout()
+    setData(null)
+  }
+
+  if (isLoading || isIdle) return <FullPageSpinner />
+
+  if (isError) {
+    return (
+      <div>
+        <p>Uh ol....</p>
+        <pre>{error.message}</pre>
+      </div>
+    )
+  }
+
+  if (isSuccess) {
+    return user ? (
+      <AuthenticatedApp user={user} logout={logout} />
+    ) : (
+      <UnauthenticatedApp login={login} register={register} />
+    )
+  }
 }
 
 export {App}
